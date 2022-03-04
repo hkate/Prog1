55281fd73b032c4ccea00a47748e297f6f2e289d
hkate <90506754+hkate@users.noreply.github.com>
2022-03-04 18:03:46 +0100

    Update drill13.cpp

--- drills/week02/drill13.cpp
+++ drills/week02/drill13.cpp
@@ -16,25 +16,8 @@ int main()
     Simple_window win (t1, 1000, 800, "My window"); //elóállttom magát az ablakom
     win.wait_for_button(); // vár addig amig megnyomja a gombot
 
-    int x_size = 800;
-    int y_size = 800;
-    int x_grid = 100;
-    int y_grid = 100;
 
     Lines grid;
-    for (int x = x_grid; x < x_size; x+=x_grid) {
-        grid.add(Point{x, 0}, Point{x, y_size});
-    }
-    for (int y = y_grid; y < y_size; y+=y_grid) {
-        grid.add(Point{0, y}, Point{x_size, y});
-    }
-
-    win.attach(grid);
-    win.wait_for_button();
-    grid.set_color(Color::red);
-    win.wait_for_button();
-
-    /*Lines grid;
     int x_size = 800;
     int y_size = 800;
     for (int i=100; i<=x_size; i+=100) {
@@ -43,8 +26,9 @@ int main()
     }
     win.attach(grid);
     win.wait_for_button() ;
-    */
 
+    int x_grid = 100;
+    int y_grid = 100;
 
     Vector_ref<Rectangle> rectangles;
         for (int i = 0; i < x_size; i+=x_grid) {
@@ -54,42 +38,36 @@ int main()
             win.attach(rectangles[rectangles.size()-1]);
         }
 
-    /*for (int t=0; t<8; t++) {
-            Graph_lib::Rectangle rect{Point{t * 100, t * 100}, 100, 100};
-            rect.set_color(Color::red);
-            rect.set_fill_color(Color::red); //néqyzetek átlósan pirosak
-            win.attach(rect);               //kattintásra egy négyzettel tovább
-            win.wait_for_button();
-    }
     win.wait_for_button();
-    */
-    Image kep1 (Point(200,0),"week02/kep.jpg");
+
+
+    Image kep1 (Point(200,0),"kep.jpg");
     win.attach(kep1);
-    Image kep2 (Point(500,200),"week02/kep.jpg");
+    Image kep2 (Point(500,200),"kep.jpg");
     win.attach(kep2);
-    Image kep3 (Point(100,500),"week02/kep.jpg");
+    Image kep3 (Point(100,500),"kep.jpg");
     win.attach(kep3);
-    
+
     win.wait_for_button();
 
     while (true)
         {
-            int randx = rand() % 7 + 1;
-            randx *= 100; 
-            int randy = rand() % 7 + 1; 
-            randy *= 100; 
-            Image kep4 (Point(randx,randy),"week02/kep.jpg");
+            int randx = rand() % 7 + 1;     // v2 in the range 1 to 8
+            randx *= 100;
+            int randy = rand() % 7 + 1;     //8x8 már kilógna a grid-ből
+            randy *= 100;
+            Image kep4 (Point(randx,randy),"kep.jpg");
             kep4.set_mask(Point(0,0),100,100);
             win.attach(kep4);
             win.wait_for_button();
         }
-        
+
         win.wait_for_button();
 
 
     } catch (exception& e) {
         cerr << "exception: " << e.what()<< '\n';
-        return 1; 
+        return 1;
     } catch (...) {
         cerr << "error\n";
         return 2;
