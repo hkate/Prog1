f717401aa4f4ab55f058de8c1dbd1c3da84b7354
hkate <90506754+hkate@users.noreply.github.com>
2022-05-29 13:25:03 +0200

    change

--- drills/week08/drill21_vector.cpp
+++ drills/week08/drill21_vector.cpp
@@ -1,61 +1,100 @@
 #include <iostream>
-#include <fstream>
-#include <string>
+#include <stdexcept>
 #include <vector>
+#include <fstream>
 #include <numeric>
 #include <algorithm>
-#include <functional>
+#include <cstdlib>
+#include <iterator>
 
 using namespace std;
 
-template<class T>
-ostream& operator<<(ostream& os, vector<T>& vector) {
-    os << "{ ";
-    for (const auto& e : vector)
-        os << e << " ";
-    os << "}";
-    return os;
+class Less_than{
+    double v;
+    public: Less_than(double vv) : v{vv}{}
+    bool operator()(const double val) { return val < v; }
+};
+
+template<typename C>
+void print(const C& c, char sep = '\n'){
+    cout << "Container elements: " << endl;
+
+    for(const auto elem: c){
+        cout << elem << sep;
+    }
+
+    cout << endl;
 }
 
-int main() {
-    vector<double> vd;
-    ifstream fileIn {"data2.txt"};
-    double value;
-    while (fileIn >> value)
-        vd.push_back(value);
-    
-    std::cout << vd << std::endl;
+int main(){
+    try{
 
-    vector<int> vi (vd.size());
-    //for (double d : vd)
-    //    vi.push_back(d);
-    std::copy(vd.begin(), vd.end(), vi.begin());
+        const string iname {"input_floats.txt"};
+        ifstream ifs {iname};
+        if(!ifs) throw runtime_error("Could not read the file: "+iname);
 
-    std::cout << "pairs of elements: " << std::endl;
-    for (int i = 0; i < vd.size(); i++)
-        std::cout << vd[i] << "\t: " << vi[i] << std::endl;
+        vector<double> vd;
+        for (double d; ifs >> d; )
+        {
+            vd.push_back(d);
+        }
+        print(vd);
 
-    double doubleSum = std::accumulate(vd.begin(), vd.end(), 0.0);
-    //for (double d : vd)
-    //    doubleSum += d;
-    std::cout << std::endl << "Sum of double vector: " << doubleSum << std::endl << std::endl;
+        vector<int> vi(vd.size());
 
-    int intSum = std::accumulate(vi.begin(), vi.end(), 0);
-    //for (int i : vi)
-    //    intSum += i;
-    double diff_sum = std::inner_product(vd.begin(), vd.end(), vi.begin(), 0.0, std::plus<double>(), std::minus<double>());
-    std::cout << "Difference of sums: " << (doubleSum - intSum) << " " << diff_sum << std::endl << std::endl;
+        /*for (auto& i : vd){
+            vi.push_back(i);
+        }*/
 
-    std::reverse(vd.begin(), vd.end());
-    std::cout << "Reverse double vector: " << vd << std::endl << std::endl;
+        copy(vd.begin(), vd.end(), vi.begin());
+        print(vi);
 
-    double vdMean = doubleSum / vd.size();
-    std::cout << "Double vector mean: " << vdMean << std::endl << std::endl;
+        for(int i=0; i<vd.size(); i++){
+            cout << vd[i] << '\t' << vi[i] << endl;
+        }
 
-    vector<double> vd2;
-    auto it = std::copy_if(vd.begin(), vd.end(), std::back_inserter(vd2), [vdMean](double value) { return value < vdMean; });
-    std::cout << "Values below mean: " << vd2 << std::endl << std::endl;
+        cout << endl;
 
-    std::sort(vd.begin(), vd.end());
-    std::cout << "Sorted double array: " << vd << std::endl;
+        double sum_of_vd_elements= accumulate(vd.begin(), vd.end(), 0.0);
+        cout << "Sum of the elements of vd: " << sum_of_vd_elements << endl;
+
+        int sum_of_vi_elements= accumulate(vi.begin(), vi.end(), 0);
+        cout << "Sum of the elements of vi: " << sum_of_vi_elements << endl;
+
+        double diff = abs(sum_of_vd_elements-sum_of_vi_elements);
+        cout << "Difference between two sum: " << diff << endl;
+
+        reverse(vd.begin(),vd.end());
+
+        cout << endl;
+        print(vd);
+
+        double mean = sum_of_vd_elements / vd.size();
+        cout << "Mean value of vd: " << mean << endl;
+
+        vector<double> vd2;
+
+        //copy_if(vd.begin(), vd.end(), vd2.begin(), [=](auto i){return i<mean;});
+
+        //auto it = copy_if(vd.begin(), vd.end(), vd2.begin(), Less_than(mean));
+        //vd2.resize(distance(vd2.begin(), it));
+
+        for(int i=0; i<vd.size(); i++){
+            if(vd[i]<mean)  vd2.push_back(vd[i]);
+        }
+
+        cout << endl;
+        print(vd2);
+
+        sort(vd.begin(), vd.end());
+
+        print(vd);
+
+    }catch(exception& e){
+        cerr << e.what() << '\n';
+        return 1;
+    }catch(...){
+        cerr << "Unknown exception\n";
+        return 2;
+    }
 }
